7a6416d95429b8ad6d8c914e941d81c13191f714
Guy Taylor <thebigguy.co.uk@gmail.com>
2012-03-07 07:08:00 +0000

    Add some licenses

--- device/gdb_proxy.py
+++ device/gdb_proxy.py
@@ -1,4 +1,20 @@
 """
+Copyright 2011 Guy Taylor <guy@thebiggerguy.com>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
+"""
 
 http://davis.lbl.gov/Manuals/GDB/gdb_31.html
 http://www.embecosm.com/appnotes/ean4/embecosm-howto-rsp-server-ean4-issue-2.html#sec_exchange_target_remote
--- device/src/main.c
+++ device/src/main.c
@@ -1,11 +1,23 @@
 /**************************************************************************//**
  * @file
- * @brief Magic
+ * @brief The Main entry point into the program.
  * @author Guy Taylor
- * @version 0.0.1
+ * @version 0.1.1
  ******************************************************************************
  * @section License
- * <b>(C) Copyright 2011-2012 Guy Taylor
+ * Copyright 2011 Guy Taylor <guy@thebiggerguy.com>
+ * 
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  *****************************************************************************/
 
 #include <stdlib.h>
